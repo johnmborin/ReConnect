426945dbe5e15bcc173bd9cf33f4f38f87805b61
Adam Myhand <rhematicmovement@gmail.com>
2023-11-26 14:27:12 -0600

    refactored the SQL

--- database.sql
+++ database.sql
@@ -1,81 +1,78 @@
+CREATE TABLE "user" (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    state VARCHAR(255) NOT NULL,
+    city VARCHAR(255) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    password VARCHAR(1000) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    access_level VARCHAR(255) NOT NULL
+);
 
-TABLE user {
-    id SERIAL [pk]
+CREATE TABLE family (
+    id SERIAL PRIMARY KEY,
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-    first_name VARCHAR(255) [NOT NULL]
-    last_name VARCHAR(255) [NOT NULL]
-    state VARCHAR(255) [NOT NULL]
-    city VARCHAR(255) [NOT NULL]
-    date_of_birth DATE [NOT NULL]
-    password VARCHAR(1000) [NOT NULL]
-    email VARCHAR(255) [NOT NULL]
-    access_level VARCHAR(255) [NOT NULL]
-}
+);
 
-TABLE user_family {
-    id SERIAL [pk]
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP 
-    user_id INT [ref: > user.id]
-    family_id INT [ref: > family.id]
-}
+CREATE TABLE user_family (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    user_id INT REFERENCES "user"(id),
+    family_id INT REFERENCES family(id)
+);
 
-TABLE family {
-    id SERIAL [pk]
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP 
-}
-
-TABLE event {
-    id SERIAL [pk]
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP 
-    date DATE [NOT NULL]
-    detail TEXT
-    family_id INT [ref: > family.id]
-    time CURRENT_TIMESTAMP
+CREATE TABLE event (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    date DATE NOT NULL,
+    detail TEXT,
+    family_id INT REFERENCES family(id),
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     type VARCHAR(255)
-}
+);
 
+CREATE TABLE user_event (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    event_id INT REFERENCES event(id),
+    user_id INT REFERENCES "user"(id),
+    attending BOOLEAN NOT NULL
+);
 
-TABLE user_event {
-    id SERIAL [pk]
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP 
-    event_id INT [ref: > event.id]
-    user_id INT [ref: > user.id]
-    attending BOOLEAN [NOT NULL]
-}
+CREATE TABLE resource (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    access_level VARCHAR(255) NOT NULL
+);
 
-TABLE resource {
-  id SERIAL [pk]
-  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP 
-  access_level VARCHAR(255) [NOT NULL] 
-  }
+CREATE TABLE question (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
 
-TABLE response {
-    id SERIAL [pk]
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP 
-    user_id INT [ref: > user.id]
-    date DATE [NOT NULL]
-    question_id INT
-    response TEXT
-    score INT 
-}
+CREATE TABLE response (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    user_id INT REFERENCES "user"(id),
+    date DATE NOT NULL,
+    question_id INT REFERENCES question(id),
+    response TEXT,
+    score INT
+);
 
-TABLE question {
-   id SERIAL [pk]
-   created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP 
-}
-
-TABLE journal {
-    id SERIAL [pk]
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP 
-    prompt_id INT [ref: > prompt.id]
-    user_id INT [ref: > user.id]
-    date DATE [NOT NULL]
+CREATE TABLE prompt (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
     detail TEXT
-}
+);
 
-TABLE prompt
-{
-    id SERIAL [pk]
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP 
+CREATE TABLE journal (
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    prompt_id INT REFERENCES prompt(id),
+    user_id INT REFERENCES "user"(id),
+    date DATE NOT NULL,
     detail TEXT
-}
+);
