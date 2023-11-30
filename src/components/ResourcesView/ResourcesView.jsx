af8d6bc7538da698c91f6d40591ebe46c834ce6a
alexafleming <alexaflemingmn@gmail.com>
2023-11-29 20:20:55 -0600

    got links to display from the database

--- src/components/ResourcesView/ResourcesView.jsx
+++ src/components/ResourcesView/ResourcesView.jsx
@@ -1,66 +1,31 @@
 import React from 'react';
-import { useState } from 'react';
+import { useSelector , useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 import './ResourcesView.css'
 
 function ResourcesView() {
-    const [isChild, setIsChild] = useState(false);
+    const dispatch = useDispatch();
+    const resources = useSelector((store) => store.resource);
+
+    useEffect(() => {
+        dispatch({ type: 'FETCH_RESOURCE' })
+    }, []);
 
     return (
         <div className='rescources-container'>
             <h2>RESOURCES</h2>
             <hr className='line-under-resources'></hr>
-
-            {isChild ? (
-                // CHILD LINKS
-                <div>
-                    <div>
-                        {/* <p>text</p> */}
-                        <a className='link' href="https://kids.kiddle.co/Divorce">
-                            Divorce Facts For Kids
-                        </a>
-                        <hr className='line-under-resources'></hr>
+            {
+                resources.map(resource => (
+                    <div class="row">
+                        
+                        <div >
+                            <a className='link' href={resource.url} target="_blank">{resource.description}</a>
+                            <hr className='line-under-resources'></hr>
+                        </div>
                     </div>
-                    <div>
-                        {/* <p>text</p> */}
-                        <a className='link' href="https://themarriagefoundation.org/effects-of-divorce-on-children/?gclid=CjwKCAiAmZGrBhAnEiwAo9qHifncaYm7sHzJiw8RR-JnEcioNxx7gO70nvG725g3pDXNv1nPMgDrDxoCQI0QAvD_BwE">
-                            Effects of Divorce on Children
-                        </a>
-                        <hr className='line-under-resources'></hr>
-                    </div>
-                    <div>
-                        {/* <p>text</p> */}
-                        <a className='link' href="https://themarriagefoundation.org/effects-of-divorce-on-children/?gclid=CjwKCAiAmZGrBhAnEiwAo9qHifncaYm7sHzJiw8RR-JnEcioNxx7gO70nvG725g3pDXNv1nPMgDrDxoCQI0QAvD_BwE">
-                            Effects of Divorce on Children
-                        </a>
-                        <hr className='line-under-resources'></hr>
-                    </div>
-                </div>
-            ) : (
-                 // PARENT LINKS
-                 <div>
-                 <div>
-                     {/* <p>text</p> */}
-                     <a className='link' href="https://themarriagefoundation.org/effects-of-divorce-on-children/?gclid=CjwKCAiAmZGrBhAnEiwAo9qHifncaYm7sHzJiw8RR-JnEcioNxx7gO70nvG725g3pDXNv1nPMgDrDxoCQI0QAvD_BwE">
-                         Effects of Divorce on Children
-                     </a>
-                     <hr className='line-under-resources'></hr>
-                 </div>
-                 <div>
-                     {/* <p>text</p> */}
-                     <a className='link' href="https://afterdivorce.goodsamaritananglican.org/?page=healingafterdivorce">
-                         How to Heal From Divorce
-                     </a>
-                     <hr className='line-under-resources'></hr>
-                 </div>
-                 <div>
-                     {/* <p>text</p> */}
-                     <a className='link' href="https://themarriagefoundation.org/effects-of-divorce-on-children/?gclid=CjwKCAiAmZGrBhAnEiwAo9qHifncaYm7sHzJiw8RR-JnEcioNxx7gO70nvG725g3pDXNv1nPMgDrDxoCQI0QAvD_BwE">
-                         Supporting Kids During Divorce
-                     </a>
-                     <hr className='line-under-resources'></hr>
-                 </div>
-             </div>
-            )}
+                ))
+            }
         </div>
     );
 }
