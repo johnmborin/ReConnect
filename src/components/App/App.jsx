86b4719f55847ac057af8f1f3f3147789c55075e
alexafleming <alexaflemingmn@gmail.com>
2023-12-03 19:37:30 -0600

    main broken fixed missing syntex

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -85,7 +85,7 @@ function App() {
             exact
             path="/login"
           >
-            {user.id ?
+            {user.id ?(
               // If the user is already logged in, 
               // redirect to the /user page
               <Redirect to="/user" />
