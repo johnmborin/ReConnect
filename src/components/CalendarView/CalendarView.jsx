25b381fc18e1da021f1131504db4fb0dbd2152f3
alexafleming <alexaflemingmn@gmail.com>
2023-12-08 20:58:52 -0600

    calendar updates

--- src/components/CalendarView/CalendarView.jsx
+++ src/components/CalendarView/CalendarView.jsx
@@ -11,6 +11,16 @@ import { StaticDatePicker } from "@mui/x-date-pickers/StaticDatePicker";
 import { PickersDay } from "@mui/x-date-pickers/PickersDay";
 import Badge from "@mui/material/Badge";
 import CheckIcon from "@mui/icons-material/Check";
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
+import { Button } from "@mui/material";
+import { TextField } from "@mui/material";
 
 // Custom Day component with Badge
 function CustomDay(props) {
@@ -36,9 +46,26 @@ function CustomDay(props) {
     </Badge>
   );
 }
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
 
 // Main component for the StaticDatePicker with Events
-export default function StaticDatePickerWithEvents() {
+function CalanderView() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   // Redux dispatch hook
   const dispatch = useDispatch();
 
@@ -73,64 +100,117 @@ export default function StaticDatePickerWithEvents() {
   }, [selectedDate]);
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      {/* StaticDatePicker with custom day rendering */}
+    <div className="calendar-page">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        {/* StaticDatePicker with custom day rendering */}
 
-      <h2>All Events</h2>
-      {events.map((event, index) => (
-        <div key={index}>
-          {event.date} - {event.detail} ({event.time})
-        </div>
-      ))}
+        <Button sx={{
+          backgroundColor: '#12646a',
+          color: "white",
+        }} variant="contained" onClick={handleClickOpen}>
+          All Events
+        </Button>
+        <BootstrapDialog
+          onClose={handleClose}
+          aria-labelledby="customized-dialog-title"
+          open={open}
+        >
+          <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+            All Events
+          </DialogTitle>
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+          <DialogContent dividers>
+            {events.map((event, index) => (
+              <Typography key={index}>
+                {event.date} - {event.detail} ({event.time})
+              </Typography>
+            ))}
+          </DialogContent>
+          <DialogActions>
+            <Button autoFocus onClick={handleClose}>
+              Save changes
+            </Button>
+          </DialogActions>
+        </BootstrapDialog>
 
-      <h2>Selected Date</h2>
-      <div>{selectedDate.format("MM/DD/YYYY")}</div>
+        <DialogActions>
 
-      <h2>Add Event</h2>
-      <input type="text" id="detail" />
-      <input type="time" id="time" />
-      <button
-        onClick={() =>
-          dispatch({
-            type: "POST_EVENT",
-            payload: {
-              date: selectedDate.format("MM/DD/YYYY"),
-              detail: document.getElementById("detail").value,
-              time: document.getElementById("time").value,
+
+        </DialogActions>
+
+        <StaticDatePicker
+          orientation="portrait"
+          value={selectedDate}
+          onChange={handleDateChange}
+          onMonthChange={handleDateChange}
+          slots={{
+            day: CustomDay,
+          }}
+          slotProps={{
+            // Pass highlighted days to CustomDay for badge rendering
+            day: {
+              highlightedDays: events
+                .filter(
+                  (event) =>
+                    dayjs(event.date).isSame(dayjs(selectedDate), "month") &&
+                    dayjs(event.date).isSame(dayjs(selectedDate), "year")
+                )
+                .map((event) => dayjs(event.date).date()),
             },
-          })
-        }
-      >
-        Add Event
-      </button>
+          }}
+        />
 
-      <StaticDatePicker
-        orientation="portrait"
-        value={selectedDate}
-        onChange={handleDateChange}
-        onMonthChange={handleDateChange}
-        slots={{
-          day: CustomDay,
-        }}
-        slotProps={{
-          // Pass highlighted days to CustomDay for badge rendering
-          day: {
-            highlightedDays: events
-              .filter(
-                (event) =>
-                  dayjs(event.date).isSame(dayjs(selectedDate), "month") &&
-                  dayjs(event.date).isSame(dayjs(selectedDate), "year")
-              )
-              .map((event) => dayjs(event.date).date()),
-          },
-        }}
-      />
-      {/* Display events for the selected date */}
-      {selectedEvents.map((event, index) => (
-        <div key={index}>
-          {event.detail} ({event.time})
+
+        <div className="add-event">
+          <TextField fullWidth type="text" id="detail" label="Event Details" variant="outlined" />
+          <div>
+            <TextField type="time" id="time" sx={{
+              paddingTop: "10px",
+            }} variant="outlined" />
+            <Button sx={{
+              height: '55px',
+              marginTop: "10px",
+              marginLeft: "10px",
+              backgroundColor: '#EFCD3E',
+              color: "white",
+            }} variant="contained "
+              onClick={() =>
+                dispatch({
+                  type: "POST_EVENT",
+                  payload: {
+                    date: selectedDate.format("MM/DD/YYYY"),
+                    detail: document.getElementById("detail").value,
+                    time: document.getElementById("time").value,
+                  },
+                })
+              }
+            >
+              ADD
+            </Button>
+          </div>
         </div>
-      ))}
-    </LocalizationProvider>
+
+        <h2>Events for {dayjs(selectedDate).format("MM/DD/YYYY")}:</h2>
+        {/* Display events for the selected date */}
+        {selectedEvents.map((event, index) => (
+          <div key={index}>
+            {event.detail} @ {dayjs(event.time).format('h:mm A')}
+          </div>
+        ))}
+      </LocalizationProvider>
+    </div>
   );
 }
+
+export default CalanderView;
